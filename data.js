75300a7061072acc424118ded02cc80bf9fb1083
Joshua van Garderen <99046134@mydavinci.nl>
2020-03-25 14:23:42 +0100

    no message

---
+++ data.js
@@ -0,0 +1,129 @@
+var parties = [{
+    name: "VVD",
+    secular: true,
+    size: 33,
+    long: "Volkspartij voor Vrijheid en Democratie"
+  },
+  {
+    name: "CDA",
+    secular: false,
+    size: 19,
+    long: "Christen Democratisch Appel"
+  },
+  {
+    name: "PVV",
+    secular: true,
+    size: 20,
+    long: "Partij voor de Vrijheid"
+  },
+  {
+    name: "D66",
+    secular: true,
+    size: 19,
+    long: "Democratie 66"
+  },
+  {
+    name: "GroenLinks",
+    secular: true,
+    size: 14
+  },
+  {
+    name: "SP",
+    secular: true,
+    size: 14,
+    long: "Socialistische Partij"
+  },
+  {
+    name: "PvdA",
+    secular: true,
+    size: 9,
+    long: "Partij van de Arbeid"
+  },
+  {
+    name: "ChristenUnie",
+    secular: false,
+    size: 5
+  },
+  {
+    name: "Partij voor de Dieren",
+    secular: true,
+    size: 33
+  },
+  {
+    name: "SGP",
+    secular: false,
+    size: 33,
+    long: "Staatkundig Gerefomeerde Partij"
+  },
+  {
+    name: "DENK",
+    secular: true,
+    size: 3
+  },
+  {
+    name: "Forum voor Democratie",
+    secular: true,
+    size: 2
+  },
+  {
+    name: "Lokaal in de Kamer",
+    secular: true,
+    size: 0
+  },
+  {
+    name: "OndernemersPartij",
+    secular: true,
+    size: 0
+  },
+  {
+    name: "VNL",
+    secular: true,
+    size: 0
+  },
+  {
+    name: "Nieuwe Wegen",
+    secular: true,
+    size: 0
+  },
+  {
+    name: "De Burger Beweging",
+    secular: true,
+    size: 0
+  },
+  {
+    name: "Piratenpartij",
+    secular: true,
+    size: 0
+  },
+  {
+    name: "Artikel 1",
+    secular: true,
+    size: 0
+  },
+  {
+    name: "Libertarische Partij",
+    secular: true,
+    size: 0
+  },
+  {
+    "name": "50Plus",
+    "secular": true,
+    "size": 2
+  },
+  {
+    "name": "Vrijzinnige Partij",
+    "secular": true,
+    "size": 0
+  },
+  {
+    "name": "Libertarische Partij",
+    "secular": true,
+    "size": 0
+  },
+  {
+    "name": "Niet Stemmers",
+    "secular": true,
+    "size": 0
+  }
+
+]
---
+++ script.js
@@ -0,0 +1,18 @@
+var title = document.getElementById("title");
+var agree =  document.getElementById("agreeButton");
+var neither =  document.getElementById("neitherButton");
+var disagree =  document.getElementById("disagreeButton");
+var skip = document.getElementById("skipButton");
+
+function loadPage() {
+  bodyparties.style.display="none";
+  agree.style.display="none";
+  neither.style.display="none";
+  disagree.style.display="none";
+  skip.style.display="none";
+  back.style.display="none";
+  extra.style.display="none";
+  end.style.display="none";
+  secular.style.display="none";
+  size.style.display="none";
+} loadPage()
